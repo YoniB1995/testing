832e29cbfb71003fd398a1cd8cccf92ed3588f5a
Yoni <yonatansamfisher@gmail.com>
2021-03-29 18:25:26 +0300

    updated

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ class User {
 }
 const usersArray = [];
 
-function newTable(){
+function showTable(){
     if (cards.style.display === 'none'){
         cards.style.display = 'flex';
         usersTable.style.display = 'none';
@@ -20,6 +20,25 @@ function newTable(){
         usersTable.style.display = 'block';
     }
     
+}
+
+function showForm(){
+     cards.style.display = 'none';
+     newForm.innerHTML = `<form>
+                    <label for="text">Full Name:<input type="text" name="fullName" id="nameInput" required></label>
+                    <br>
+                    <label for="email">Email:<input type="email"  id="emailInputOne" required></label>
+                    <br>
+                    <label for="email">Email:<input type="email"  id="emailInputTwo" required></label>
+                    <br>
+                    <label for="age">Age: <input type="number"  id="ageInput" required></label>
+                    <br>
+                    <label for="telephone">Phone: <input type="tel"  id="phoneInput" required></label><br> <button onclick="submitNewUser()">Submit</button>
+                </form>`;
+}
+
+function submitNewUser(){
+    emailInputOne.value === emailInputTwo.value ?  true : alert("Not same password, try again");
 }
 
 fetch(`https://next.json-generator.com/api/json/get/NJ-UoW2Xq`)
@@ -61,132 +80,20 @@ function back(){
     newCardPage.innerHTML = '';
 }
 
-
 function getCard(num){
-    switch (num) {
-        case 0:
+    for (a=0;a<usersArray.length;a++){
+        if (num == a){
             cards.style.display = 'none';
             newCardPage.innerHTML = `<div class='cardNewPage'>
             <div class='cardLeft'><h3>User Details</h3>
-            <h5>Full Name: ${usersArray[0].name}<h5>
-            <h5>Email: ${usersArray[0]['email']}<h5>
-            <h5>Age: ${usersArray[0]['age']}</h5>
-            <h5>Phone: ${usersArray[0]['phone']}<h5>
+            <h5>Full Name: ${usersArray[a].name}<h5>
+            <h5>Email: ${usersArray[a]['email']}<h5>
+            <h5>Age: ${usersArray[a]['age']}</h5>
+            <h5>Phone: ${usersArray[a]['phone']}<h5>
             </div>
-            <div class='cardRight'><img src=${usersArray[0].picture} alt="user_picture"></div><button onclick='back()'>Return</button>
+            <div class='cardRight'><img src=${usersArray[a].picture} alt="user_picture"></div><button onclick='back()'>Return</button>
             </div>`;
             break;
-        case 1:
-            cards.style.display = 'none';
-            newCardPage.innerHTML = `<div class='cardNewPage'>
-            <div class='cardLeft'><h3>User Details</h3>
-            <h5>Full Name: ${usersArray[1].name}<h5>
-            <h5>Email: ${usersArray[1]['email']}<h5>
-            <h5>Age: ${usersArray[1]['age']}</h5>
-            <h5>Phone: ${usersArray[1]['phone']}<h5>
-            </div>
-            <div class='cardRight'><img src=${usersArray[1].picture} alt="user_picture"></div><button onclick='back()'>Return</button>
-            </div>`;
-            break;
-        case 2:
-            cards.style.display = 'none';
-            newCardPage.innerHTML = `<div class='cardNewPage'>
-            <div class='cardLeft'><h3>User Details</h3>
-            <h5>Full Name: ${usersArray[2].name}<h5>
-            <h5>Email: ${usersArray[2]['email']}<h5>
-            <h5>Age: ${usersArray[2]['age']}</h5>
-            <h5>Phone: ${usersArray[2]['phone']}<h5>
-            </div>
-            <div class='cardRight'><img src=${usersArray[2].picture} alt="user_picture"></div><button onclick='back()'>Return</button>
-            </div>`;
-            break;
-        case 3:
-            cards.style.display = 'none';
-            newCardPage.innerHTML = `<div class='cardNewPage'>
-            <div class='cardLeft'><h3>User Details</h3>
-            <h5>Full Name: ${usersArray[3].name}<h5>
-            <h5>Email: ${usersArray[3]['email']}<h5>
-            <h5>Age: ${usersArray[3]['age']}</h5>
-            <h5>Phone: ${usersArray[3]['phone']}<h5>
-            </div>
-            <div class='cardRight'><img src=${usersArray[3].picture} alt="user_picture"></div><button onclick='back()'>Return</button>
-            </div>`;
-            break;
-        case 4:
-            cards.style.display = 'none';
-            newCardPage.innerHTML = `<div class='cardNewPage'>
-            <div class='cardLeft'><h3>User Details</h3>
-            <h5>Full Name: ${usersArray[4].name}<h5>
-            <h5>Email: ${usersArray[4]['email']}<h5>
-            <h5>Age: ${usersArray[4]['age']}</h5>
-            <h5>Phone: ${usersArray[4]['phone']}<h5>
-            </div>
-            <div class='cardRight'><img src=${usersArray[4].picture} alt="user_picture"></div><button onclick='back()'>Return</button>
-            </div>`;
-            break;
-        case 5:
-            cards.style.display = 'none';
-            newCardPage.innerHTML = `<div class='cardNewPage'>
-            <div class='cardLeft'><h3>User Details</h3>
-            <h5>Full Name: ${usersArray[5].name}<h5>
-            <h5>Email: ${usersArray[5]['email']}<h5>
-            <h5>Age: ${usersArray[5]['age']}</h5>
-            <h5>Phone: ${usersArray[5]['phone']}<h5>
-            </div>
-            <div class='cardRight'><img src=${usersArray[5].picture} alt="user_picture"></div><button onclick='back()'>Return</button>
-            </div>`;
-            break;
-        case 6:
-            cards.style.display = 'none';
-            newCardPage.innerHTML = `<div class='cardNewPage'>
-            <div class='cardLeft'><h3>User Details</h3>
-            <h5>Full Name: ${usersArray[6].name}<h5>
-            <h5>Email: ${usersArray[6]['email']}<h5>
-            <h5>Age: ${usersArray[6]['age']}</h5>
-            <h5>Phone: ${usersArray[6]['phone']}<h5>
-            </div>
-            <div class='cardRight'><img src=${usersArray[6].picture} alt="user_picture"></div><button onclick='back()'>Return</button>
-            </div>`;
-            break;
-        case 7:
-            cards.style.display = 'none';
-            newCardPage.innerHTML = `<div class='cardNewPage'>
-            <div class='cardLeft'><h3>User Details</h3>
-            <h5>Full Name: ${usersArray[7].name}<h5>
-            <h5>Email: ${usersArray[7]['email']}<h5>
-            <h5>Age: ${usersArray[7]['age']}</h5>
-            <h5>Phone: ${usersArray[7]['phone']}<h5>
-            </div>
-            <div class='cardRight'><img src=${usersArray[7].picture} alt="user_picture"></div><button onclick='back()'>Return</button>
-            </div>`;
-            break;
-        case 8:
-            cards.style.display = 'none';
-            newCardPage.innerHTML = `<div class='cardNewPage'>
-            <div class='cardLeft'><h3>User Details</h3>
-            <h5>Full Name: ${usersArray[8].name}<h5>
-            <h5>Email: ${usersArray[8]['email']}<h5>
-            <h5>Age: ${usersArray[8]['age']}</h5>
-            <h5>Phone: ${usersArray[8]['phone']}<h5>
-            </div>
-            <div class='cardRight'><img src=${usersArray[8].picture} alt="user_picture"></div><button onclick='back()'>Return</button>
-            </div>`;
-            break;
-        case 9:
-            cards.style.display = 'none';
-            newCardPage.innerHTML = `<div class='cardNewPage'>
-            <div class='cardLeft'><h3>User Details</h3>
-            <h5>Full Name: ${usersArray[9].name}<h5>
-            <h5>Email: ${usersArray[9]['email']}<h5>
-            <h5>Age: ${usersArray[9]['age']}</h5>
-            <h5>Phone: ${usersArray[9]['phone']}<h5>
-            </div>
-            <div class='cardRight'><img src=${usersArray[9].picture} alt="user_picture"></div><button onclick='back()'>Return</button>
-            </div>`;
-            break;
-    
-        default:
-            break;
+        }
     }
 }
-
