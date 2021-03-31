8867583d795dd3605e4ea96287f0e0b09b6cfee5
Yoni <yonatansamfisher@gmail.com>
2021-03-31 19:39:42 +0300

    updated final

--- script.js
+++ script.js
@@ -14,7 +14,6 @@
 }
 animationLoader();
 
-
 class User {
     constructor(id,picture,age,name,email,phone){
         this.id= id;
@@ -42,29 +41,24 @@ function showTable(){
 }
 
 function showForm(){
-     cards.style.display = 'none';
-     newForm.innerHTML = `<form class='userForm'>
-     <h2 style="color:black">User Registration</h2>
-                    <label for="text">Full Name:<input type="text" name="fullName" id="nameInput" required></label>
-                    <br>
-                    <label for="email">Email:<input type="email"  id="emailInputOne" required></label>
-                    <br>
-                    <label for="email">Confirm Email:<input type="email"  id="emailInputTwo" required></label>
-                    <br>
-                    <label for="age">Age: <input type="number"  id="ageInput" required></label>
-                    <br>
-                    <label for="telephone">Phone: <input type="tel"  id="phoneInput" required></label><br> <button onclick="submitNewUser()">Submit</button><a href="index.html"><button class="returnBtn" type="button" >Return</button></a>
-                </form>`
-                ;
-                
+    cards.style.display = 'none';
+    newForm.innerHTML = `<form class='userForm'>
+    <h2 style="color:black">User Registration</h2>
+    <label for="text">Full Name:<input type="text" name="fullName" id="nameInput" required></label>
+    <br>
+    <label for="email">Email:<input type="email"  id="emailInputOne" required></label>
+    <br>
+    <label for="email">Confirm Email:<input type="email"  id="emailInputTwo" required></label>
+    <br>
+    <label for="age">Age: <input type="number"  id="ageInput" required></label>
+    <br>
+    <label for="telephone">Phone: <input type="tel"  id="phoneInput" required></label><br> <button onclick="submitNewUser()">Submit</button><a href="index.html"><button class="returnBtn" type="button" >Return</button></a>
+    </form>`;
 }
-
 
 function submitNewUser(){
-    
     emailInputOne.value === emailInputTwo.value ?  alert('Account Added!') : alert("Not same password, try again");
 }
-
 
 fetch(`https://next.json-generator.com/api/json/get/NJ-UoW2Xq`)
 .then((usersList)=> {
@@ -80,23 +74,17 @@ cardDetails[a].innerHTML = `<h3>User Details</h3>
  <h5>Phone: ${usersDetails[a]['phone']}<h5>
  <h4>Picture:</h4><img src="${usersDetails[a]['picture']}" alt="user_picture">`;
 
-
 tableAdd.innerHTML += `<tr>
  <td>${usersDetails[a]['name'].first} ${usersDetails[a]['name'].last}</td>
 <td>${usersDetails[a]['age']}</td>
 <td>${usersDetails[a]['email']}</td>
 <td>${usersDetails[a]['phone']}</td>
-</tr>`
-}
+</tr>`}
 })
-
-
-
 
 function sendEmailUpdates(){
     emailInput.value = ''
     alert('Email Sent!');
-
 }
 
 function getWhatsAppNum(){
@@ -110,28 +98,26 @@ function goToMainPage(){
 
 function getCardDetails(num){
     for (a=0;a<usersArray.length;a++){
-        newCardPage.style.display = 'none'
+    newCardPage.style.display = 'none'
         if (num == a){
-            smallLoader.style.display = '';
-            mainBody.style.height = '50vh'
+        smallLoader.style.display = '';
+        mainBody.style.height = '50vh'
     const smallLoadingGif = ()=> {
-     setTimeout(()=>{
-        mainBody.style.height = '';
-        smallLoader.style.display = 'none'
-        newCardPage.style.display = ''
+    setTimeout(()=>{
+    mainBody.style.height = '';
+    smallLoader.style.display = 'none'
+    newCardPage.style.display = ''
     },1000)}
-smallLoadingGif();
-            cards.style.display = 'none';
-            newCardPage.innerHTML = `<div class='cardNewPage'>
-            <div class='cardLeft'><h3>User Details</h3>
-            <h5>Full Name: ${usersArray[a].name}<h5>
-            <h5>Email: ${usersArray[a]['email']}<h5>
-            <h5>Age: ${usersArray[a]['age']}</h5>
-            <h5>Phone: ${usersArray[a]['phone']}<h5>
-            </div>
-            <div class='cardRight'><img src=${usersArray[a].picture} alt="user_picture"></div><button onclick='goToMainPage()'>Return</button>
-            </div>`;
-            break;
-        }
-    }
+    smallLoadingGif();
+    cards.style.display = 'none';
+    newCardPage.innerHTML = `<div class='cardNewPage'>
+    <div class='cardLeft'><h3>User Details</h3>
+    <h5>Full Name: ${usersArray[a].name}<h5>
+    <h5>Email: ${usersArray[a]['email']}<h5>
+    <h5>Age: ${usersArray[a]['age']}</h5>
+    <h5>Phone: ${usersArray[a]['phone']}<h5>
+    </div>
+    <div class='cardRight'><img src=${usersArray[a].picture} alt="user_picture"></div><button onclick='goToMainPage()'>Return</button>
+    </div>`;
+    break;}}
 }
